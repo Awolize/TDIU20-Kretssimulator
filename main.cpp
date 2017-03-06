23f9d779c24790a244659d5fdec57fc7be7cf9e6
Kristoffer Karlsson <krika694@student.liu.se>
2017-03-06 16:47:47 +0100

    update

--- main.cpp
+++ main.cpp
@@ -1,4 +1,51 @@
+#include <iostream>
+#include "Component.h"
 
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    cout << argv[] << endl;
+    int cycles, writes, volt;
+    double iterations;
+    for (int i{0}; i < argc; i++;)
+    {
+	if (i == 1)
+	{
+	    cycles = argv[i];
+	}
+	else if (i == 2)
+	{
+	    iterations = argv[i];
+	}
+	else if (i == 3)
+	{
+	    volts = argv[i];
+	}
+	else if (i == 4)
+	{
+	    writes = argv[i];
+	}
+	else 
+	{
+	    cout << arg[i] << " Is not defined." << endl;
+	    break;
+	}
+    }
+    
+    
+  Connection p, n;
+  vector<Component*> net;
+  net.push_back(new Battery("Bat", 24.0, p, n));
+  net.push_back(new Resistor("R1", 6.0, p, n));
+  net.push_back(new Resistor("R2", 8.0, p, n));
+  net.push_back(new Capacitor("C1", 0.0047, p , n));
+  //simulate(net, cycles, volt, writes);
+    
+    
+    
+    return 0;
+}
 
 
 /*
@@ -10,9 +57,10 @@
   net.push_back(new Resistor("R1", 6.0, p, n));
   net.push_back(new Resistor("R2", 8.0, p, n));
   simulate(net, 10000, 10, 0.1);
-*/
+
 
 
 
 Bat. -> .R1. -> .bat
    . -> .R2. -> . 
+*/
